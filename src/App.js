3058745f2cdc0b280d4a805fde89df90a9371952
Sujitha221102 <sujithatheivam@gmail.com>
2023-08-01 11:49:10 +0530

    blog

--- src/App.js
+++ src/App.js
@@ -9,33 +9,7 @@ function App() {
       <NavBar />
       <hr />
       <Content />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <P />
-        <MyBtn/>
-      </header> */}
     </div>
   );
 }
-// function P(){
-//   return(
-//     <p>This is React Library</p>
-//   )
-// }
-// function MyBtn(){ 
-//   return(
-//     <button>Press Here</button>
-//   )
-// }
 export default App;
